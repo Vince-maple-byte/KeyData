90500f733b9ef8620bfe1214812dab87c00fe5dc
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2026-03-10 00:07:31 -0400

    Made the search and insert methods for the skip list

--- internals/file/skiplist.go
+++ internals/file/skiplist.go
@@ -2,16 +2,71 @@ package file
 
 import "math/rand/v2"
 
-var MAX_LEVEL int = 32; 
+const MAX_LEVEL = 32; 
 
 type Node struct {
 	key string
+	value []byte
 	levels []*Node
 }
 
 type Skiplist struct {
 	head *Node
-	
+}
+
+func CreateSkiplist() *Skiplist {
+	head := &Node{
+		key: "",
+		value: nil,
+		levels: make([]*Node, MAX_LEVEL),
+	};
+	return &Skiplist{head: head};
+}
+
+func (list *Skiplist) Search(key string) []byte {
+	curr := list.head;
+	var next *Node;
+
+	for i := len(list.head.levels)-1; i >= 0; i-- {
+		
+		for next = curr.levels[i]; next != nil && key > next.key; next = curr.levels[i] {
+			curr = next;
+		}
+		if next != nil && next.key == key {
+			return next.value;
+		}
+	} 
+
+	return nil;
+}
+
+func (list *Skiplist) Insert(key string, value []byte) {
+	var update [MAX_LEVEL]*Node;
+	curr := list.head;
+
+	for i:= MAX_LEVEL-1; i >= 0; i-- {
+		for curr.levels[i] != nil && key > curr.levels[i].key {
+			curr = curr.levels[i];
+		}
+
+		update[i] = curr;
+	}
+
+	newNode := &Node{
+		key: key, 
+		value: value,
+		levels: make([]*Node, randLevel()),
+	}
+
+	for i := 0; i < len(newNode.levels); i++ {
+		newNode.levels[i] = update[i].levels[i];
+		update[i].levels[i] = newNode;
+	}
+}
+
+func (list *Skiplist) Delete(key string) []byte {
+
+	return nil;
 }
 
 func randLevel() int {
